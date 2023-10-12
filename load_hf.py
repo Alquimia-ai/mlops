d08c8ea39a71a9f63beaf03590b867c307737533
Alex Fiorenza <alexfiorenza2012@gmail.com>
2023-10-12 15:02:33 -0300

    Serving model to api

--- load_hf.py
+++ load_hf.py
@@ -1,6 +1,35 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import os
 import mlflow.pytorch
+import torch.nn.functional as F
+import torch
+# Define the Custom PyFunc Model
+import mlflow.pyfunc
+
+class HuggingFaceWrapper(mlflow.pyfunc.PythonModel):
+
+    def load_context(cls, context):
+        cls.model = AutoModelForSequenceClassification.from_pretrained(context.artifacts["model"])
+        cls.tokenizer = AutoTokenizer.from_pretrained(context.artifacts["tokenizer"])
+
+    def predict(self, context, model_input):
+        # Extract text data from the DataFrame
+        texts = model_input["text"].tolist()
+
+        # Tokenize
+        inputs = self.tokenizer(texts, return_tensors="pt", truncation=True, padding=True)
+
+        # Get model's outputs
+        with torch.no_grad():
+            outputs = self.model(**inputs).logits
+
+        # Apply the softmax to get probabilities
+        probabilities = torch.nn.functional.softmax(outputs, dim=1)
+
+        # Get the predicted class label
+        predicted_classes = probabilities.argmax(dim=1).tolist()
+        
+        return predicted_classes
 
 
 model_name = "alexFiorenza/intent_classification_js"  # replace with your model's identifier on Hugging Face
@@ -12,6 +41,12 @@ model.save_pretrained("hf_model")
 tokenizer.save_pretrained("hf_model")
 
 
-# Start an MLflow run and log the model
 with mlflow.start_run() as run:
-    mlflow.pytorch.log_model(model, "model", registered_model_name="intent_classification_js")
+    artifacts = {
+        "model": "hf_model",
+        "tokenizer": "hf_model"
+    }
+
+    model_info=mlflow.pyfunc.log_model(artifact_path="hf_model_custom",
+                            python_model=HuggingFaceWrapper(),
+                            artifacts=artifacts)
--- serve.sh
+++ serve.sh
@@ -1,1 +1,1 @@
-mlflow models serve -m models:/intent_classification_js/Production -p 1234
+mlflow models serve -m runs:/f58d52138c70487d901d6a99b201aeab/hf_model_custom -p 1234
